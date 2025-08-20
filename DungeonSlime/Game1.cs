c2fa0390cea63f46be316ef9d7de7f79b41d17e0
syf <2654525303@qq.com>
2025-08-20 20:41:29 +0800

    handling input

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -11,6 +11,9 @@ public class Game1() : Core("Dungeon Slime", 1280, 720, false)
     private AnimatedSprite _slime = null!;
     private AnimatedSprite _bat = null!;
 
+    private Vector2 _slimePosition;
+    private const float MovementSpeed = 5;
+
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
@@ -20,12 +23,6 @@ public class Game1() : Core("Dungeon Slime", 1280, 720, false)
 
     protected override void LoadContent()
     {
-        // var atlasTexture = Content.Load<Texture2D>("images/atlas");
-        // var atlas = new TextureAtlas { Texture = atlasTexture };
-
-        // atlas.AddRegion("slime", new Rectangle(0, 0, 20, 20));
-        // atlas.AddRegion("bat", new Rectangle(20, 0, 20, 20));
-
         var atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
 
         _slime = atlas.CreateAnimatedSprite("slime-animation");
@@ -45,7 +42,85 @@ public class Game1() : Core("Dungeon Slime", 1280, 720, false)
         _slime.Update(gameTime);
         _bat.Update(gameTime);
 
+        CheckKeyboardInput();
+        CheckGamePadInput();
+
         base.Update(gameTime);
+    }
+
+    private void CheckKeyboardInput()
+    {
+        var keyboardState = Keyboard.GetState();
+
+        var speed = MovementSpeed;
+        if (keyboardState.IsKeyDown(Keys.Space))
+        {
+            speed *= 1.5f;
+        }
+
+        if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+        {
+            _slimePosition.Y -= speed;
+        }
+
+        if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+        {
+            _slimePosition.Y += speed;
+        }
+
+        if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+        {
+            _slimePosition.X -= speed;
+        }
+
+        if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+        {
+            _slimePosition.X += speed;
+        }
+    }
+
+    private void CheckGamePadInput()
+    {
+        var gamePadState = GamePad.GetState(PlayerIndex.One);
+
+        var speed = MovementSpeed;
+        if (gamePadState.IsButtonDown(Buttons.A))
+        {
+            speed *= 1.5f;
+            GamePad.SetVibration(PlayerIndex.One, 1f, 1f);
+        }
+        else
+        {
+            GamePad.SetVibration(PlayerIndex.One, 0, 0);
+        }
+
+        // thumbstick has priority
+        if (gamePadState.ThumbSticks.Left != Vector2.Zero)
+        {
+            _slimePosition.X += gamePadState.ThumbSticks.Left.X * speed;
+            _slimePosition.Y -= gamePadState.ThumbSticks.Left.Y * speed; // invert Y
+            return;
+        }
+
+        if (gamePadState.IsButtonDown(Buttons.DPadUp))
+        {
+            _slimePosition.Y -= speed;
+        }
+
+        if (gamePadState.IsButtonDown(Buttons.DPadDown))
+        {
+            _slimePosition.Y += speed;
+        }
+
+        if (gamePadState.IsButtonDown(Buttons.DPadLeft))
+        {
+            _slimePosition.X -= speed;
+        }
+
+        if (gamePadState.IsButtonDown(Buttons.DPadRight))
+        {
+            _slimePosition.X += speed;
+        }
     }
 
     protected override void Draw(GameTime gameTime)
@@ -54,7 +129,7 @@ public class Game1() : Core("Dungeon Slime", 1280, 720, false)
 
         using (SpriteBatch.DrawContext(samplerState: SamplerState.PointClamp))
         {
-            _slime.Draw(SpriteBatch, Vector2.One);
+            _slime.Draw(SpriteBatch, _slimePosition);
             _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
         }
 
